8c66fa96882794579609217bba6f188f1d6db4e3
skim <Sua.Kim2@sony.com>
2024-11-20 16:51:32 -0800

    script for json validation

---
+++ validate_json.py
@@ -0,0 +1,24 @@
+import os
+import json
+from jsonschema import validate, Draft7Validator
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+if __name__ == "__main__":
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    schema_path = os.path.join(base_path, 'schemas/output_main_schema.json')
+    sample_path = os.path.join(base_path, 'schemas/output_sample.json')
+
+    main_schema = read_json_file(schema_path)
+    sample = read_json_file(sample_path)
+    
+    try:
+        # validate(instance=sample, schema=main_schema)
+        Draft7Validator(main_schema).validate(sample)
+        print("The JSON sample is valid")
+    except Exception as e:
+        print("The JSON sample is invalid:", e)

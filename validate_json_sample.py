b336a911d5154f270be619439930f718d149cbbf
skim <Sua.Kim2@sony.com>
2024-11-20 17:41:12 -0800

    handled relative paths

--- validate_json_sample.py
+++ validate_json_sample.py
@@ -1,6 +1,6 @@
 import os
 import json
-from jsonschema import validate, Draft7Validator
+from jsonschema import validate, Draft7Validator, RefResolver
 
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
@@ -10,15 +10,16 @@ def read_json_file(file_path):
 if __name__ == "__main__":
 
     base_path = os.path.dirname(os.path.abspath(__file__))
-    schema_path = os.path.join(base_path, 'schemas/config_main_schema.json')
-    sample_path = os.path.join(base_path, 'sample/config_sample.json')
+    schema_path = os.path.join(base_path, 'schemas/output_main_schema.json')
+    sample_path = os.path.join(base_path, 'sample/output_sample.json')
 
     main_schema = read_json_file(schema_path)
     sample = read_json_file(sample_path)
     
     try:
         # validate(instance=sample, schema=main_schema)
-        Draft7Validator(main_schema).validate(sample)
+        resolver = RefResolver(base_uri="file:///C:/Users/3000095175/Documents/codes/eval-ai-models/", referrer=True)
+        Draft7Validator(main_schema, resolver=resolver).validate(sample)
         print("The JSON sample is valid")
     except Exception as e:
         print("The JSON sample is invalid:", e)

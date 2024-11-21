fe1237224f90d27cd41ab2de0208efbb457b753d
skim <Sua.Kim2@sony.com>
2024-11-20 17:03:51 -0800

    Update validate_json_sample.py

--- validate_json_sample.py
+++ validate_json_sample.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     base_path = os.path.dirname(os.path.abspath(__file__))
     schema_path = os.path.join(base_path, 'schemas/config_main_schema.json')
-    sample_path = os.path.join(base_path, 'schema_sample/config_sample.json')
+    sample_path = os.path.join(base_path, 'sample/config_sample.json')
 
     main_schema = read_json_file(schema_path)
     sample = read_json_file(sample_path)
